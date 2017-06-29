4a463ffe363d59eb4618e29f9b81ace10d4b01ec
wsiewert <wsiewert94@gmail.com>
2017-06-29 16:39:36 -0500

    added classes

---
+++ RockPaperScissors/RockPaperScissors/PlayerHuman.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class PlayerHuman
+    {
+        public string name;
+        public int score;
+
+        public PlayerHuman()
+        {
+            name = "";
+            score = 0;
+        }
+
+        public void IncrementScore()
+        {
+            score++;
+        }
+
+        public void PlayerChoice()
+        {
+            
+        }
+    }
+}
--- RockPaperScissors/RockPaperScissors/Program.cs
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -10,6 +10,25 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("ROCK PAPER SCISSORS LIZARD SPOCK");
+            Console.WriteLine("'1' - Player vs AI");
+            Console.WriteLine("'2' - Player vs player");
+
+            string userInput = Console.ReadLine().ToString().Trim();
+
+            switch (userInput)
+            {
+                case "1":
+                    PlayerVAI playerVAI = new PlayerVAI();
+                    playerVAI.StartGame();
+                    break;
+                case "2":
+                    PlayerVPlayer playerVPlayer = new PlayerVPlayer();
+                    playerVPlayer.StartGame();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
